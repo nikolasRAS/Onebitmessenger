5026592e1b0b1f10e335c8f7bce1d0241e962490
Nikolas RAS <nikolas.ras@gmail.com>
2019-12-12 20:24:36 +0000

    Aula 02 - Onebitmessenger

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,10 +7,21 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+
 import bulmaQuickview from 'bulma-quickview/src/js'
 
 document.addEventListener('turbolinks:load', function() {
   let quickview = bulmaQuickview.attach()
+  
+  const fileInput = document.querySelectorAll('input[type=file].file-input')
+  fileInput.forEach(input => {
+    input.onchange = () => {
+      if (input.files.length > 0) {
+        const fileName = input.parentNode.querySelector(".file-name")
+        fileName.textContent = input.files[0].name
+      }
+    }
+  })
 })
 
 
